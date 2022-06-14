287e75c6c76b5fdb648e0fc0af01516215396890
AnneComp <101005212+AnneComp@users.noreply.github.com>
2022-06-14 06:02:09 +0530

    Add files via upload

---
+++ Computer_Vision_.py
@@ -0,0 +1,165 @@
+import cv2
+import math
+from cv2 import waitKey
+import cv2.aruco as aruco
+import numpy as np
+import os
+from pytest import approx
+#######################
+img= cv2.imread("Python/CVtask.jpg")
+img_copy=img.copy()
+img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img_edges=cv2.Canny(img_gray,70,70)
+_,thresh=cv2.threshold(img_gray,200,255,cv2.THRESH_BINARY)
+
+aruco_list=['Python/Ha.jpg','Python/LMAO.jpg','Python/XD.jpg','Python/HaHa.jpg']
+aruco_id_dict={}
+
+color_val={'green':[79,209,146],'orange':[9,127,240],'black':[0,0,0],'pink/peach':[210,22,228]}
+color_id={'green':1,'orange':2,'black':3,'pink/peach':4}
+#######################
+def main():
+    img_copy_u=img.copy()
+    global aruco_id_dict
+    for i in aruco_list:
+        x=cv2.imread(i)
+        (a,b,c)=find_aruco_prop(x)
+        aruco_id_dict[i]=b
+    get_squares(img_edges)
+    cv2.imshow("Contour",img)
+    #dddd=cv2.resize(img,(640,480))
+    #cv2.imshow('ff',dddd)
+    ddee=cv2.resize(img_copy_u,(640,480))
+    cv2.imshow('ffgg',ddee)
+    cv2.waitKey(0)
+#######################
+def find_aruco_prop(the_img):
+    gray_img=cv2.cvtColor(the_img,cv2.COLOR_BGR2GRAY)
+    key=getattr(aruco,f'DICT_5X5_250')
+    arucoDict=aruco.Dictionary_get(key)
+    arucoParam=aruco.DetectorParameters_create()
+    (corners,id_s,rejected)=cv2.aruco.detectMarkers(gray_img,arucoDict,parameters=arucoParam)
+    return corners,id_s,rejected
+#######################
+def co_ord_aruco(an_aruco):
+    corners,ids,r=find_aruco_prop(an_aruco)
+    if len(corners)>0:
+        ids=ids.flatten()
+        for (markerCorner,markerIds) in zip(corners,ids):
+            corners=markerCorner.reshape((4,2))
+            (topLeft,topRight,bottomRight,bottomLeft)=corners
+            topLeft=(int(topLeft[0]),int(topLeft[1]))
+            topRight=(int(topRight[0]),int(topRight[1]))
+            bottomRight=(int(bottomRight[0]),int(bottomRight[1]))
+            bottomLeft=(int(bottomLeft[0]),int(bottomLeft[1]))
+        return topLeft,topRight,bottomLeft,bottomRight
+#######################
+def crop_aruco(ar_img):
+    topLeft,topRight,bottomLeft,bottomRight=co_ord_aruco(ar_img)
+    print('\nTL:',topLeft,'\nTR:',topRight,'\nBL:',bottomLeft,'\nBR:',bottomRight,'\nx:',ar_img.shape[1])
+    crp_img=ar_img[topLeft[1]:topRight[1],topLeft[0],bottomLeft[0]]
+    crp_cor=np.array([[0,0],[crp_img.shape[1],0],[crp_img.shape[1],crp_img.shape[0]],[0,crp_img.shape[0]]])
+    ar_cor=np.array([[0,0],[ar_img.shape[1],0],[ar_img.shape[1],ar_img.shape[0]],[0,ar_img.shape[0]]])
+    waitKey(0)
+    return ar_img,ar_cor
+#######################
+def aruco_angle(aruco_img):
+    topLeft,topRight,bottomLeft,bottomRight=co_ord_aruco(aruco_img)
+    centre_x=int((topLeft[0]+bottomRight[0])/2.0)
+    centre_y=int((topLeft[1]+bottomRight[1])/2.0)
+    centre=(centre_x,centre_y)
+    line_x=int((topLeft[0]+topRight[0])/2.0)
+    line_y=int((topLeft[1]+topRight[1])/2.0)
+    line_mid=(line_x,line_y)
+    if line_x!=centre_x:
+        ar_angle=(math.atan((centre_y-line_y)/(centre_x-line_x)))*180/math.pi
+    else:
+        ar_angle=90
+    return centre,line_mid,ar_angle
+#######################
+def rotate(thetha,aruco_centre,aruco_image):
+    M=cv2.getRotationMatrix2D(aruco_centre,thetha,1.0)
+    ar_img=cv2.warpAffine(aruco_image,M,aruco_image.shape[1::-1])
+    ar_cor=np.array([[0,0],[ar_img.shape[1],0],[ar_img.shape[1],ar_img.shape[0]],[0,ar_img.shape[0]]])
+    return ar_img,ar_cor
+#######################
+def ord_of_co_ord(sm_list):
+    x_min=sm_list[0][0]
+    x_max=sm_list[0][0]
+    y_min=sm_list[0][1]
+    y_max=sm_list[0][1]
+    for i in sm_list:
+        if i[0]>x_max:
+            x_max=i[0]
+        if i[1]>y_max:
+            y_max=i[1]
+        if i[0]<x_min:
+            x_min=i[0]
+        if i[1]<y_min:
+            y_min=i[1]
+    return x_min,y_min,x_max,y_max
+#######################
+def overlay_aruco(or_img,pix_val,sq_size,rot_aruco):
+    upd_img=np.array(or_img)
+    marker=np.array(rot_aruco)
+    resized_aruco=cv2.resize(marker,dsize=(sq_size[1],sq_size[0]))
+    
+    or_img[pix_val[0]:pix_val[1],pix_val[2]:pix_val[3]]=resized_aruco
+
+#######################
+def get_squares(imgex):
+    global color_val,color_id
+    global aruco_id_dict
+    contours,heirarchy=cv2.findContours(imgex,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        cv2.drawContours(img_copy,cnt,-1,(0,255,0),2)
+        list_for_sq=[]
+        perimeter=cv2.arcLength(cnt,True)
+        approxx=cv2.approxPolyDP(cnt,0.02*perimeter,True)
+        x,y,w,h=cv2.boundingRect(approxx)
+        if len(approxx)==4:
+            find_square=w/float(h)
+            if find_square>0.95 and find_square<1.05:
+                objectType='Square'
+                co_ord=[all_co[0].tolist() for all_co in approxx]
+                xmin,ymin,xmax,ymax=ord_of_co_ord(co_ord)
+                sq_img=imgex[ymin:ymax,xmin:xmax]
+                sq_shape=sq_img.shape
+                sq_x=sq_shape[0]
+                sq_y=sq_shape[1]
+                mid=(int((co_ord[0][0]+co_ord[1][0])/2),int((co_ord[0][1]+co_ord[1][1])/2))
+                centre=(int((co_ord[0][0]+co_ord[2][0])/2),int((co_ord[0][1]+co_ord[2][1])/2))
+                if centre[0]!=mid[0]:
+                    theta=(math.atan((centre[1]-mid[1])/(centre[0]-mid[0])))*180/math.pi
+                else:
+                    theta=90             
+                for i in color_val.keys():
+                    vals=np.array(color_val[i])
+                    vals.reshape((3,))
+                    if (vals==img_copy[centre[1],centre[0],:]).any():
+                        abc=np.array(color_id[i])
+                        abc.reshape((1,1))
+                        for j in aruco_id_dict.keys():
+                            if (abc==aruco_id_dict[j]).any():
+                                id_=aruco_id_dict[j]
+                                pt1=approxx
+                                xx=j
+                                mm=cv2.imread(xx)
+                                cen,lm,ar_an=aruco_angle(mm)                                
+                                updated_aruco,pt2=rotate(ar_an,cen,mm)
+                                crop_aruco(updated_aruco)                           
+                                #print('pt2:',pt2,'pt1: ',pt1,'lllllp\n')
+                                M,_=cv2.findHomography(pt2,pt1)
+                                global img
+                                warp_img=cv2.warpPerspective(updated_aruco,M,(img.shape[1],img.shape[0]))
+                                cv2.fillConvexPoly(img,pt1,(0,0,0))
+                                img=img+warp_img                                
+                                waitKey(0)
+                list_for_sq.append(approxx)
+            else: objectType='Rect'
+        else: objectType='None'
+        
+        cv2.putText(img_copy,objectType,[x+(w//2)-5,y+(h//2)],cv2.FONT_HERSHEY_COMPLEX,0.5,(255,0,0),2)
+    return list_for_sq
+#######################
+main()
